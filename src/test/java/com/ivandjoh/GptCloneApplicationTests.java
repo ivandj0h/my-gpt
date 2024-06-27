90b97ac08deb9f1e413ca28b83f3b082bbde8400
ivandjoh <ivandi.djoh@gmail.com>
2024-06-27 10:02:13 +0800

    init

---
+++ src/test/java/com/ivandjoh/GptCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandjoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
